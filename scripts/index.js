0fd10fd2bcc42b43b2660cffd99f02f6f62a14e9
Thadeu Cotts <thadeu@feracode.com>
2020-10-19 23:21:47 -0300

    [add] : method to fetch data from API

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,19 @@ const { createObjectCsvWriter } = require('csv-writer')
 const APIKEY = '4803588b326265fdef50becfb8eedbdd'
 const PATH = '../data/monthlyPrices.csv'
 
+/**
+ *
+ * @param {String} key - APIKEY to be used to fetch file information
+ * Using axios to fetch data and return the value of request information
+ * as a JSON formart
+ */
+async function fetchData(key) {
+  try {
+    const response = await axios.get(
+      `http://api.eia.gov/series/?api_key=${key}&series_id=NG.RNGWHHD.M`
+    )
+    return response.data.series[0].data
+  } catch (error) {
+    throw new Error('Error On Fetch Information.')
+  }
+}
