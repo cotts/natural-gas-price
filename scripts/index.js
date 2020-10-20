55fd3405fa2d7bf4890a8fdbede65dda4d9d527e
Thadeu Cotts <thadeu@feracode.com>
2020-10-19 23:21:10 -0300

    [add] : main script requirements

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+const axios = require('axios')
+const { createObjectCsvWriter } = require('csv-writer')
+
+const APIKEY = '4803588b326265fdef50becfb8eedbdd'
+const PATH = '../data/monthlyPrices.csv'
+
